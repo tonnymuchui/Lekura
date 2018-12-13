5152122fef815e080ae4580a29d2895aeb54351d
tonnymuchui <tonykanyingah@gmail.com>
2018-12-14 00:18:28 +0300

    Auth activities

---
+++ app/src/main/java/com/lekura/lekura/Auth/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.lekura.lekura.Auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lekura.lekura.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
