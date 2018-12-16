d88735a2ae21b530caa0470853cc0a8ce32fadfc
tonnymuchui <tonykanyingah@gmail.com>
2018-12-17 01:37:10 +0300

    setting up profile information

--- app/src/main/java/com/lekura/lekura/Chat/ChatActivity.java
+++ app/src/main/java/com/lekura/lekura/Chat/ChatActivity.java
@@ -1,19 +1,29 @@
 package com.lekura.lekura.Chat;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.lekura.lekura.Auth.LoginActivity;
+import com.lekura.lekura.Auth.SignupActivity;
 import com.lekura.lekura.R;
+import com.lekura.lekura.SettingActivity;
 
 public class ChatActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser user;
+    private DatabaseReference rootReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,7 @@ public class ChatActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
-
+        rootReference = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
@@ -44,8 +54,49 @@ public class ChatActivity extends AppCompatActivity {
 
         }
         if (item.getItemId() == R.id.main_find_Settings_option){
+            sendUserToSettingActivity();
 
         }
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (user == null){
+            Intent signUp= new Intent(ChatActivity.this,SignupActivity.class);
+            signUp.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(signUp);
+            finish();
+        }
+        else {
+            veryfyUser();
+        }
+    }
+    private void veryfyUser() {
+        String currentUserId = auth.getCurrentUser().getUid();
+        rootReference.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if ((dataSnapshot.child("name").exists())){
+                    Toast.makeText(ChatActivity.this,"Welcome",Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                    sendUserToSettingActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void sendUserToSettingActivity(){
+        Intent settingIntent = new Intent(ChatActivity.this,SettingActivity.class);
+        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(settingIntent);
+        finish();
     }
 }
--- app/src/main/java/com/lekura/lekura/SettingActivity.java
+++ app/src/main/java/com/lekura/lekura/SettingActivity.java
@@ -1,13 +1,127 @@
 package com.lekura.lekura;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.lekura.lekura.Auth.LoginActivity;
+import com.lekura.lekura.Chat.ChatActivity;
+
+import java.util.HashMap;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingActivity extends AppCompatActivity {
+    @BindView(R.id.Update_button) Button Update_button;
+    @BindView(R.id.set_profile_image) CircleImageView set_profile_image;
+    @BindView(R.id.set_status_name) EditText set_status_name;
+    @BindView(R.id.set_user_name) EditText set_user_name;
+    private String currentUser;
+    private FirebaseAuth sauth;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        ButterKnife.bind(this);
+
+        sauth = FirebaseAuth.getInstance();
+        currentUser = sauth.getCurrentUser().getUid();
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        Update_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateSetting();
+            }
+        });
+//        RetriveUserData();
     }
+
+    private void updateSetting() {
+        String setUserName = set_user_name.getText().toString();
+        final String setStatus = set_status_name.getText().toString();
+
+        if (TextUtils.isEmpty(setUserName)){
+            Toast.makeText(this,"Enter Your Name",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            HashMap<String,String> profileMap= new HashMap<>();
+            profileMap.put("uid",currentUser);
+            profileMap.put("name",setUserName);
+            profileMap.put("Status",setStatus);
+            reference.child("Users").child(currentUser).setValue(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                sendUserToChatActivity();
+                                Toast.makeText(SettingActivity.this,"Profile Complete",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingActivity.this,"Error" + message,Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+    private void sendUserToChatActivity(){
+        Intent mainIntent = new Intent(SettingActivity.this,ChatActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+//    private void RetriveUserData() {
+//
+//        reference.child("Users").child(currentUser)
+//                .addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name") && (dataSnapshot.hasChild("image")))){
+//
+//                            String retriveUserName = dataSnapshot.child("name").getValue().toString();
+//                            String retriveStatus = dataSnapshot.child("status").getValue().toString();
+//                            String retriveProfilePhoto = dataSnapshot.child("image").getValue().toString();
+//
+//                            set_user_name.setText(retriveUserName);
+//                            set_status_name.setText(retriveStatus);
+//
+//                        }
+//                        else if ((dataSnapshot.exists()) && (dataSnapshot.hasChild("name"))){
+//
+//                            String retriveUserName = dataSnapshot.child("name").getValue().toString();
+//                            String retriveStatus = dataSnapshot.child("status").getValue().toString();
+//
+//                            set_user_name.setText(retriveUserName);
+//                            set_status_name.setText(retriveStatus);
+//
+//                        } else {
+//                            Toast.makeText(SettingActivity.this,"Update profile",Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                    }
+//                });
+//    }
 }
