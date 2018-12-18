b3f0329095d82395435cfe6ddbf78840b1889170
tonnymuchui <tonykanyingah@gmail.com>
2018-12-18 17:45:36 +0300

    Created group chat activity

---
+++ app/src/main/java/com/lekura/lekura/Chat/GroupChatActivity.java
@@ -0,0 +1,15 @@
+package com.lekura.lekura.Chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lekura.lekura.R;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+    }
+}
